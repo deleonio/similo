095a97d3c04b9acbb6e7c688bb8129571e63f420
Martin Oppitz <m.oppitz@modevel.de>
2019-04-26 06:44:14 +0200

    Add TestAdapter

---
+++ src/test.js
@@ -0,0 +1,81 @@
+// @flow
+
+import { ComponentDeclarationType } from './adapter/types';
+
+import {
+  Component,
+  Directive,
+  Injectable,
+  Inject,
+  Input,
+  Output,
+  Pipe
+} from '@angular/core';
+import {
+  VERSION
+} from '@angular/common';
+import {
+  AdapterInterface
+} from './adapter/interface';
+import {
+  Adapter
+} from './adapter';
+
+export class TestAdapter extends Adapter implements AdapterInterface {
+
+  constructor() {
+    super(null);
+    this.name = 'Test';
+    this.version = '0.0.0';
+  }
+
+  component(): Function {
+    return (): Function => {
+      return () => {
+      }
+    }
+  }
+
+  directive(): Function {
+    return (): Function => {
+      return () => {
+      }
+    }
+  }
+
+  inject(): Function {
+    return (): Function => {
+      return () => {
+      }
+    }
+  }
+
+  injectable(): Function {
+    return (): Function => {
+      return () => {
+      }
+    }
+  }
+
+  input(): Function {
+    return (): Function => {
+      return () => {
+      }
+    }
+  }
+
+  output(): Function {
+    return (): Function => {
+      return () => {
+      }
+    }
+  }
+
+  pipe(): Function {
+    return (): Function => {
+      return () => {
+      }
+    }
+  }
+
+}
