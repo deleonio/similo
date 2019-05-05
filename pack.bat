8d21e4bace7354cf529c15db800c83119df37bf5
Martin Oppitz <npmjs@martinoppitz.com>
2019-05-05 04:28:38 +0200

    cleanup

--- pack.bat
+++
@@ -1,1 +0,0 @@
-gulp clean && "node_modules/.bin/karma" start karma.conf.js && gulp build && npm pack
---
+++ pack.sh
@@ -0,0 +1,3 @@
+rm -rf adapter index.js angular.js angularjs.js vue.js test.js
+cp -r src/* ./
+npm pack
