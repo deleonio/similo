104e40116fd57431dbc85b849538bcfd5a2c9ba5
Martin Oppitz <npmjs@martinoppitz.com>
2019-05-27 22:49:50 +0200

    babel settings

--- babel.config.js
+++ babel.config.js
@@ -1,17 +1,7 @@
 module.exports = {
   plugins: [
-    [
-      '@babel/plugin-proposal-private-methods',
-      {
-        loose: true
-      }
-    ],
-    [
-      '@babel/plugin-proposal-class-properties',
-      {
-        loose: true
-      }
-    ],
+    '@babel/plugin-proposal-private-methods',
+    '@babel/plugin-proposal-class-properties',
     '@babel/plugin-transform-flow-strip-types'
   ],
   presets: [
