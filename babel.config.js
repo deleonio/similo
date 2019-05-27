6d1c43c39ae4369ec5c995ed2e7fdcf0b0db7361
Martin Oppitz <npmjs@martinoppitz.com>
2019-05-27 22:43:04 +0200

    babel env polyfill

--- babel.config.js
+++ babel.config.js
@@ -1,28 +1,31 @@
 module.exports = {
   plugins: [
-    // [
-    //   '@babel/plugin-proposal-decorators',
-    //   {
-    //     legacy: true
-    //   }
-    // ],
-    // [
-    //   '@babel/plugin-proposal-private-methods',
-    //   {
-    //     loose: true
-    //   }
-    // ],
     [
-      '@babel/plugin-proposal-class-properties',
+      "@babel/plugin-proposal-private-methods",
       {
         loose: true
       }
+    ],
+    [
+      "@babel/plugin-proposal-class-properties",
+      {
+        loose: true
+      }
+    ],
+    "@babel/plugin-transform-flow-strip-types"
+  ],
+  presets: [
+    [
+      "@babel/preset-env",
+      {
+        corejs: 3,
+        useBuiltIns: "entry"
+      }
     ]
   ],
-  presets: ["@babel/preset-flow"],
   env: {
     test: {
-      plugins: ['istanbul']
+      plugins: ["istanbul"]
     }
   },
   sourceMaps: true,
