15d7436341de2d22447ad9dd49217f547d2d764f
Martin Oppitz <npmjs@martinoppitz.com>
2019-05-30 07:20:53 +0200

    @babel/plugin-proposal-optional-catch-binding

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   plugins: [
-    '@babel/plugin-proposal-private-methods',
     '@babel/plugin-proposal-class-properties',
+    '@babel/plugin-proposal-optional-chaining',
+    '@babel/plugin-proposal-private-methods',
     '@babel/plugin-transform-flow-strip-types'
   ],
   presets: [
